3ef5d18f86642c834670061d9b846d8c6b8c47b9
Ben Lieberman <bhlieberman93@gmail.com>
2023-10-14 08:17:07 -0600

    major styling overhauls

--- src/main/nia/views/cantos/one/parens_five.cljs
+++ src/main/nia/views/cantos/one/parens_five.cljs
@@ -1,35 +1,31 @@
 (ns nia.views.cantos.one.parens-five
-  (:require [re-com.core :refer [hyperlink v-box]]))
+  (:require [re-com.core :refer [box h-box hyperlink]]))
 
 (defn parens-five [{:keys [navigate]}]
   [:div {:style {:display :flex
                  :flex-direction :column}}
-   [v-box :align-self :center
-    :children [[:div 
-                [:b.text-primary.display-4 "((((("]
-                [:a {:name "fn1ret"} "sometimes"]
-                [:p "a mere spark"]
-                [:p "Can, firemen climbing ladders all over it,"]
-                [:a {:href "#nia1par5.fn1"} "Reduce a peaceful building to a crater"]
-                [:b.text-primary.display-4 ")))))"]] 
-               [:hr]
-               [:p ""]
-               [hyperlink {:label "Return to thesis"
-                           :on-click #(navigate :thesis)}]
-               [hyperlink {:label "Return to parens-one"
-                           :on-click #(navigate :parens)}]
-               [hyperlink {:label "Return to parens-two"
-                           :on-click #(navigate :parens-two)}]
-               [hyperlink {:label "Return to parens-three"
-                           :on-click #(navigate :parens-three)}]
-               [hyperlink {:label "Return to parens-four"
-                           :on-click #(navigate :parens-four)}]
-               [:hr]
-               [:a {:name "nia1par5.fn1"} "1. "]
-               [:p "What one needs, when one has to put out a building on fire,"]
-               [:p "Is a swift giant - when a building is burning,"]
-               [:p "Could a confirmed life-saver, dragging his feet,"]
-               [:p "Stand back and coyly watch the frantic activity from a distance? -"]
-               [:p "Who is prepared like Gulliver to overcome his prudishness,"]
-               [:p "Relishing the opportunity"
-                [hyperlink {:label "to relieve himself"}] "."]]]])
+   [h-box
+    :style {:display :flex}
+    :align-self :center
+    :children [[box 
+                :style {:display :flex
+                        :align-items :center}
+                :child [:b.text-primary.display-4.m-3 "((((("]]
+               [box :child [:div
+                            [:a {:name "fn1ret"} "sometimes"]
+                            [:p "a mere spark"]
+                            [:p "Can, firemen climbing ladders all over it,"]
+                            [:a {:href "#nia1par5.fn1"} "Reduce a peaceful building to a crater"]
+                            [:hr]
+                            [:a {:name "nia1par5.fn1"} "1. "]
+                            [:p "What one needs, when one has to put out a building on fire,"]
+                            [:p "Is a swift giant - when a building is burning,"]
+                            [:p "Could a confirmed life-saver, dragging his feet,"]
+                            [:p "Stand back and coyly watch the frantic activity from a distance? -"]
+                            [:p "Who is prepared like Gulliver to overcome his prudishness,"]
+                            [:p "Relishing the opportunity"
+                             [hyperlink {:label "to relieve himself"}] "."]]]
+               [box 
+                :style {:display :flex
+                        :align-items :center}
+                :child [:b.text-primary.display-4.m-3 ")))))"]]]]])
--- src/main/nia/views/cantos/one/parens_two.cljs
+++ src/main/nia/views/cantos/one/parens_two.cljs
@@ -1,21 +1,27 @@
 (ns nia.views.cantos.one.parens-two
-  (:require [re-com.core :refer [hyperlink v-box]]))
+  (:require [re-com.core :refer [hyperlink v-box h-box box]]))
 
 (defn parens-two [{:keys [navigate]}]
-  [:div {:style {:display :flex
-                 :flex-direction :column}}
-   [v-box 
-    :align-self :center
-    :children [[:div [:b.text-warning.display-4 "(("] 
-                [:p "To be able to re-touch! as when,"
-                 [hyperlink {:label "wearing her jewellery"
-                             :on-click #(navigate :parens-three)}]]
-                [:p "And caught in a family snap, an aged beauty\n"]
-                [:p "Not wishing to show any wrinkles"]
-                [:p "Is changed from mother into sister on the negative;"]
-                [:b.text-warning.display-4.text-right "))"]]
-               [:hr]
-               [hyperlink {:label "Return to thesis"
-                           :on-click #(navigate :thesis)}] 
-               [hyperlink {:label "Return to parens-one"
-                           :on-click #(navigate :parens)}]]]])
+  [h-box
+   :width "750px"
+   :align-self :center
+   :style {:display :flex
+           :flex-direction :row}
+   :children
+   [[box
+     :style {:display :flex
+             :align-items :center}
+     :child [:b.text-warning.display-2.m-3 "(("]]
+    [box
+     :child [:div
+             [:p "To be able to re-touch! as when, "
+              [hyperlink {:label "wearing her jewellery"
+                          :on-click #(navigate :parens-three)}]]
+             [:p "And caught in a family snap, an aged beauty\n"]
+             [:p "Not wishing to show any wrinkles"]
+             [:p "Is changed from mother into sister on the negative;"]]]
+    [box
+     :style {:display :flex
+             :align-items :center}
+     :child [:b.text-warning.display-2.m-3 "))"]]
+]])
