8d8a01b88ac51f410ac5081e6436e3e1b341df58
Ben Lieberman <bhlieberman93@gmail.com>
2023-10-15 14:43:50 -0600

    lifted state and added children to middle

--- portfolio/nia/scenes/scroll_scene.cljs
+++ portfolio/nia/scenes/scroll_scene.cljs
@@ -1,6 +1,8 @@
 (ns nia.scenes.scroll-scene
   (:require [portfolio.reagent-18 :refer-macros [defscene]]
+            [reagent.core :as r]
             [nia.views.parens-scroll :refer [parens-scroll]]))
 
 (defscene scroll
-  [parens-scroll])
+  [parens-scroll {:children [:div "hello"]
+                  :depth (r/atom 1)}])
--- src/main/nia/views/parens_scroll.cljs
+++ src/main/nia/views/parens_scroll.cljs
@@ -7,29 +7,30 @@
              4 :success
              5 :primary})
 
-(defn parens-scroll []
-  (let [depth (r/atom 1)]
-    (fn []
-      [:div.d-flex
-       [:div
-        {:class "m-3 display-4 border"
-         :on-click (fn [] 
-                     (when (>= @depth 1) (swap! depth dec)))}
-        (doall (for [i (reverse (range 1 6))
-               :let [classname (if (> i @depth)
-                                 "text-muted "
-                                 "")]]
-           [:b {:class (some->> (get colors i :muted)
-                                name
-                                (str classname "text-"))} "("]))]
-       [:div
-        {:class "m-3 display-4 border"
-         :on-click (fn []
-                     (when (< @depth 5) (swap! depth inc)))}
-        (doall (for [i (range 1 6)
-               :let [classname (if (> i @depth)
-                                 "text-muted "
-                                 "")]]
-           [:b {:class (some->> (get colors i)
-                                name
-                                (str classname "text-"))} ")"]))]])))
+(defn parens-scroll [{:keys [children depth]}]
+  [:div.d-flex
+   [:div
+    {:class "m-3 display-4 border"
+     :on-click (fn [] 
+                 (when (>= @depth 1) (swap! depth dec)))}
+    (doall (for [i (reverse (range 1 6))
+                 :let [classname (if (> i @depth)
+                                   "text-muted "
+                                   "")]]
+             [:b {:key (str i "-left")
+                  :class (some->> (get colors i :muted)
+                                  name
+                                  (str classname "text-"))} "("]))]
+   children
+   [:div
+    {:class "m-3 display-4 border"
+     :on-click (fn []
+                 (when (< @depth 5) (swap! depth inc)))}
+    (doall (for [i (range 1 6)
+                 :let [classname (if (> i @depth)
+                                   "text-muted "
+                                   "")]]
+             [:b {:key (str i "-right")
+                  :class (some->> (get colors i)
+                                  name
+                                  (str classname "text-"))} ")"]))]])
