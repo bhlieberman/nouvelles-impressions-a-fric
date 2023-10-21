11852dd3905617eab884ec79ee387b19b3c5c31d
Ben Lieberman <bhlieberman93@gmail.com>
2023-10-21 16:43:43 -0600

    scratch

--- src/main/nia/util.cljs
+++ src/main/nia/util.cljs
@@ -8,4 +8,29 @@
 
 (def nia-1-parens (get-in raw-data [:canto 0 :parens]))
 
-(nth nia-1-parens 3)
+(def nia-2 (get-in raw-data [:canto 1]))
+
+(def nia-2-thesis (:body nia-2))
+
+(def nia-2-parens (:parens nia-2))
+
+nia-2-parens
+
+(comment 
+  ;; programmatic routes WIP
+  (for [canto [1 2 4]
+       :let [names {nil "thesis"
+                    1 "one"
+                    2 "two"
+                    3 "three"
+                    4 "four"
+                    5 "five"}]]
+   [(str "/" canto)
+    (into ["/parens"]
+          (for [i (range 1 6)
+                :let [route-name (str "nia.routing.canto." (get names i))
+                      view-name (str "nia.views.cantos." (get names i))]]
+            [(str "/" i)
+             {:name (keyword route-name (get names i))
+              :view (keyword view-name (get names i))
+              :controllers []}]))]))
