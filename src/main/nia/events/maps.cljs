38b0f75594d9263fd7c8c683da80ef808ac15bc6
Ben Lieberman <bhlieberman93@gmail.com>
2023-10-24 10:15:30 -0600

    working on maps

--- src/main/nia/events/maps.cljs
+++ src/main/nia/events/maps.cljs
@@ -1,9 +1,23 @@
 (ns nia.events.maps
   (:require [goog.object :as gobj]
             [nia.config.maps :refer [api-key]]
-            [re-frame.core :refer [reg-event-fx reg-fx]]
+            [re-frame.core :refer [dispatch reg-event-db reg-event-fx reg-fx]]
             [shadow.cljs.modern :refer [js-await]]
             ["@googlemaps/js-api-loader" :refer [Loader]]))
+
+(reg-event-db
+ :config/save-map
+ (fn [db [_ map]]
+   (assoc db :config.geo/map map)))
+
+(reg-event-db
+ :maps/set-current-center
+ (fn [db [_ new-center]]
+   (let [new-coords (get-in db [:config.maps/coords new-center] 
+                            (throw (ex-info "no mappable location" {})))]
+     (-> db
+         (assoc :config.maps/current-center new-coords)
+         (update :config.geo/map #(.setCenter %1 %2) new-coords)))))
 
 (reg-event-fx
  :config/load-google-maps
@@ -17,6 +31,7 @@
                                      :version "weekly"}))]
              (js-await [lib (js/google.maps.importLibrary "maps")]
                        (let [Map (gobj/get lib "Map")]
-                         (Map. canvas (clj->js {:center {:lat lat
-                                                         :lng lng}
-                                                :zoom zoom})))))))
+                         (dispatch [:config/save-map 
+                                    (Map. canvas (clj->js {:center {:lat lat
+                                                                    :lng lng}
+                                                           :zoom zoom}))]))))))
