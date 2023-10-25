d9ef4f8639aac0ed4db2f1d100e4e54e2458a2b0
Ben Lieberman <bhlieberman93@gmail.com>
2023-10-25 16:06:53 -0600

    remove not very insecure url

--- src/main/nia/config/storage.cljs
+++ src/main/nia/config/storage.cljs
@@ -3,7 +3,9 @@
             [shadow.cljs.modern :refer [js-await]]
             ["@azure/storage-blob" :refer [BlobServiceClient]]))
 
-(def client (BlobServiceClient. "https://roussel.blob.core.windows.net?sv=2022-11-02&ss=b&srt=o&sp=rlactfx&se=2023-10-26T05:25:22Z&st=2023-10-25T21:25:22Z&spr=https&sig=9ElYLsdt45ciUG4U2pc8wtzdo%2Fvvk4WI17HgboU3W9s%3D"))
+(goog-define sas-url "")
+
+(def client (BlobServiceClient. sas-url))
 
 (defn get-pic []
   (let [container-client (.getContainerClient client "nia")
