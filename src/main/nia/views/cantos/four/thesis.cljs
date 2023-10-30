f64a3809c5c8e4dcc28b53992e62b3397fa20ea6
Ben Lieberman <bhlieberman93@gmail.com>
2023-10-30 17:28:23 -0600

    probably going to remove maps

--- src/main/nia/views/cantos/four/thesis.cljs
+++ src/main/nia/views/cantos/four/thesis.cljs
@@ -1,30 +1,28 @@
 (ns nia.views.cantos.four.thesis
-  (:require [nia.views.maps :refer [react-google-map]]
-            [re-com.core :refer [hyperlink p title v-box]]
+  (:require [re-com.core :refer [hyperlink p title v-box]]
             [re-frame.core :refer [dispatch]]))
 
 (defn thesis []
   [v-box
    :margin "25px 0px"
-   :children 
-   [[v-box 
+   :children
+   [[v-box
      :align-self :center
      :children
      [[title {:level :level2
               :label "Canto IV"
-              :style {:align-self :center}}] 
-      [title {:level :level3 :style {:align-self :center} :label 
-              "The Gardens of Rosetta Seen from A Dahabiah"}] 
+              :style {:align-self :center}}]
+      [title {:level :level3 :style {:align-self :center} :label
+              "The Gardens of Rosetta Seen from A Dahabiah"}]
       [title {:level :level3
               :style {:align-self :center
                       :margin "10px 0px 10px 0px"}
               :label [:i "Outskirts of Cairo"]}]
-      
+
       [p "Skimming along the Nile I see two banks going past covered"]
       [p "With flowers, birds' wings, flashes of light, lush green plants,"]
       [p "One of which would suffice for twenty of our "
        [hyperlink {:label "salons"
-                   :on-click #(dispatch [:routing/push-state :nia.routing.canto.four/one {:id 4}])}]] 
+                   :on-click #(dispatch [:routing/push-state :nia.routing.canto.four/one {:id 4}])}]]
       [p "With its opaque foliage, lightly coloured stripes and fruits."]
-      [:hr]
-      [react-google-map :rosetta]]]]])
+      [:hr]]]]])
