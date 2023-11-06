ad44aa01c95772a14f2e0feef6ceb238c24697ae
Ben Lieberman <bhlieberman93@gmail.com>
2023-11-06 10:41:38 -0700

    experimenting w reg-flow

--- src/main/nia/config/storage.cljs
+++ src/main/nia/config/storage.cljs
@@ -1,3 +1,5 @@
 (ns nia.config.storage)
 
 (goog-define sas-url "")
+
+(goog-define sas-token "")
--- src/main/nia/events/storage.cljs
+++ src/main/nia/events/storage.cljs
@@ -1,13 +1,28 @@
 (ns nia.events.storage
-  (:require [nia.config.storage :refer [sas-url]]
+  (:require [nia.config.storage :refer [sas-url sas-token]]
+            [re-frame.alpha :refer [reg-flow]]
             [re-frame.core :refer [dispatch inject-cofx reg-cofx reg-event-db reg-event-fx reg-fx]]
             [shadow.cljs.modern :refer [js-await]]
             ["@azure/storage-blob" :refer [BlobServiceClient]]))
 
+(reg-flow
+ {:id ::blob-flow
+  :inputs {:image-url [:image/url]
+           :blob-client [:blob-client]}
+  :output (fn get-new-image [_ {:keys [image-url ^js blob-client]}]
+            (let [^js client (.getBlobClient blob-client image-url)
+                  url (atom nil)]
+              (js-await [^js blob (.download client)]
+                        (js-await [^js body (.-blobBody blob)]
+                                  (let [^js obj-url (js/URL.createObjectURL body)]
+                                    (reset! url obj-url))))
+              url))
+  :path [:canto :one :image]})
+
 (reg-cofx
  :blob-service-client
  (fn [cofx]
-   (assoc cofx :blob-service-client (BlobServiceClient. sas-url))))
+   (assoc cofx :blob-service-client (BlobServiceClient. (str sas-url "?" sas-token)))))
 
 (reg-event-db
  :images/create-url
@@ -19,7 +34,8 @@
  [(inject-cofx :blob-service-client)]
  (fn [{:keys [^js blob-service-client db]} _]
    (let [^js container-client (.getContainerClient blob-service-client "nia")]
-     {:db (assoc db :blob-client container-client)})))
+     {:db (assoc db :blob-client container-client)
+      :fx [[:dispatch [:storage/get-new-url "snowman.jpeg"]]]})))
 
 (reg-event-fx
  :storage/get-new-url
