2122b579ac1a58ab05146da9d63b7db4c1e6620d
Ben Lieberman <bhlieberman93@gmail.com>
2024-02-08 05:57:24 -0700

    other portfolio changes

--- portfolio/nia/scenes/scroll_scene.cljs
+++ portfolio/nia/scenes/scroll_scene.cljs
@@ -35,60 +35,71 @@
   (let [depth (r/atom 1)
         parens {0 :thesis 1 :one 2 :two 3 :three 4 :four 5 :five}]
     (fn [{:keys [children]}]
-      [:div
-       {:style {:display :flex
-                :align-items :stretch
-                :height "500px"}}
-       [button
-        {:component (.-div motion)
-         :while-hover {:background-color "rgba(128,128,128,0.1)"} 
-         :style {:height "100%" 
-                 :display :flex
-                 :align-items :center
-                 :justify-content :center
-                 :background-color "rgba(0,0,0,0)"}
-         :on-click (fn []
-                     (when (>= @depth 1)
-                       (swap! depth dec)))}
+      [h-box
+       :src (at)
+       :style {:display :flex
+               :align-items :stretch 
+               :height "500px"}
+       :children
+       [[button
+         {:component (.-div motion)
+          :while-hover {:background-color "rgba(128,128,128,0.1)"
+                        :border-radius "1rem"} 
+          :style {:height "100%" 
+                  :display :flex
+                  :align-items :center 
+                  :margin-right "10px"
+                  :justify-content :center 
+                  :background-color "rgba(0,0,0,0)"}
+          :on-click (fn []
+                      (when (>= @depth 1)
+                        (swap! depth dec)))}
 
-        [:div
-         {:style {:display :flex}}
-         (doall
-          (for [i (reverse (range 1 6))
-                :let [classname (if (> i @depth) "text-muted " "")]]
-            [title
-             :level :level1
-             :style {:align-self :center}
-             :label "("
-             :class (some->> (get colors i)
-                             name
-                             (str classname "text-"))]))]]
-       children
-       [button
-        {:component (.-div motion)
-         :variant :contained
-         :color :primary
-         :while-hover {:background-color "rgba(128,128,128,0.1)"}
-         :style {:height "100%"
-                 :display :flex 
-                 :align-items :center
-                 :justify-content :center
-                 #_#_:background-color "rgba(0,0,0,0)"}
-         :on-click (fn []
-                     (when (<= 0 @depth 4)
-                       (swap! depth inc)))} 
-        [:div
-         {:style {:display :flex}}
-         (doall
-          (for [i (range 1 6)
-                :let [classname (if (> i @depth) "text-muted " "")]]
-            [title
-             :level :level1
-             :style {:align-self :center}
-             :label ")"
-             :class (some->> (get colors i)
-                             name
-                             (str classname "text-"))]))]]])))
+         [:div
+          {:style {:display :flex
+                   :padding "2px"
+                   :margin-left "10px"
+                   :margin-right "10px"}}
+          (doall
+           (for [i (reverse (range 1 6))
+                 :let [classname (if (> i @depth) "text-muted " "")]]
+             [title
+              :level :level1
+              :style {:align-self :center}
+              :label "("
+              :class (some->> (get colors i)
+                              name
+                              (str classname "text-"))]))]]
+        children
+        [button
+         {:component (.-div motion)
+          :variant :contained
+          :color :primary
+          :while-hover {:background-color "rgba(128,128,128,0.1)"
+                        :border-radius "1rem"} 
+          :style {:height "100%"
+                  :display :flex 
+                  :align-items :center 
+                  :justify-content :center
+                  :background-color "rgba(0,0,0,0)"}
+          :on-click (fn []
+                      (when (<= 0 @depth 4)
+                        (swap! depth inc)))} 
+         [:div
+          {:style {:display :flex
+                   :align-self :center
+                   :margin-left "10px"
+                   :margin-right "10px"}}
+          (doall
+           (for [i (range 1 6)
+                 :let [classname (if (> i @depth) "text-muted " "")]]
+             [title
+              :level :level1
+              :style {:align-self :center}
+              :label ")"
+              :class (some->> (get colors i)
+                              name
+                              (str classname "text-"))]))]]]])))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defscene scroll
